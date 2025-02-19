276081e50f93089f0041a14e1d901d4b79d1db17
Kimheng-SOK <sokkimheng008@gmail.com>
2025-02-19 21:34:46 +0700

    add new tp

---
+++ script.js
@@ -0,0 +1,18 @@
+class Counter{
+    number = 0;
+    constructor(){
+        this.number = 0;
+    }
+
+    increment(){
+        this.number++;
+    }
+
+    decrement(){
+        this.number--;
+    }
+
+    reset(){
+        this.number = 0;
+    }
+}
