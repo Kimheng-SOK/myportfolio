dcbe1de3351d7574e3307957b8d3f02e0cf635de
Kimheng-SOK <sokkimheng008@gmail.com>
2025-02-19 21:45:09 +0700

    update

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ class Counter{
 
     increment(){
         this.number++;
+        console.log(this.number);
     }
 
     decrement(){
         this.number--;
+        console.log(this.number);
     }
 
     reset(){
         this.number = 0;
+        console.log(this.number);
+    }
+
+    getNumber(){
+        return this.number;
+        console.log(this.number);
     }
 }
